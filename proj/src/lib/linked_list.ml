93f4470c728e5aabb8a3e2eb9226d346d21a6a1f
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-04-16 13:06:03 +0530

    modify linked list

--- proj/src/lib/linked_list.ml
+++ proj/src/lib/linked_list.ml
@@ -1,17 +1,51 @@
-include List
+type node = { value : int; mutable next : node option }
 
-let search_list : 'a t -> 'a -> bool =
+type t = { mutable head : node option }
+
+let unwrap : 'a option -> 'a = function
+  | None -> failwith "Can't unwrap"
+  | Some x -> x
+
+let create_node : int -> node = fun x -> { value = x; next = None }
+
+let create_list : unit -> t = fun () -> { head = None }
+
+let insert_list : t -> int -> unit =
  fun lst x ->
-  match List.find_opt (fun a -> a = x) lst with None -> false | Some _ -> true
+  let node = create_node x in
 
-let insert_list : 'a t -> 'a -> 'a t =
- fun lst x -> match lst with [] -> [ x ] | lst -> rev @@ x :: rev lst
+  (* now we have a node which has a value and it points to nothing
+     to add it to the list we have to replace the head with the new node
+     at the same time attach the previous head to the next record of the new node *)
+  let old_node = lst.head in
+  (match old_node with
+  | None -> node.next <- None
+  | Some x -> node.next <- Some x);
+  lst.head <- Some node
 
-let delete_list : 'a t -> 'a -> 'a t =
- fun lst x ->
-  let rec aux acc lst =
-    match lst with
-    | [] -> failwith "This list is empty"
-    | hd :: tl -> if hd = x then append (rev acc) tl else aux (hd :: acc) tl
+let delete_list = fun lst x ->
+  let p = {head=lst.head} in
+  let rec delete_list' = fun lst ->
+    match lst.head with
+    | None -> failwith "Empty list"
+    | Some node ->
+      if (unwrap node.next).value = x then
+        node.next <- (unwrap node.next).next
+      else begin
+        p.head <- node.next;
+        delete_list' p
+      end
+    in
+  delete_list' p
+        
+
+let fmt_list : t -> string =
+ fun lst ->
+  let rec fmt_list' lst acc =
+    match lst.head with
+    | None -> List.rev acc
+    | Some node -> fmt_list' { head = node.next } (node.value :: acc)
   in
-  aux [] lst
+  List.fold_left
+    (fun acc x -> acc ^ Printf.sprintf " -> %d" x)
+    "" (fmt_list' lst [])
